7b2999ba3f8e378df4e83eb50f6b0c325209d80d
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-16 15:29:12 +0900

    必要であるバリデーションを設けた

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  
   has_one_attached :image
 end
