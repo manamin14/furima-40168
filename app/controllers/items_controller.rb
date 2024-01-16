6d004a0c1b853e5b58391c81fdf3622bf10e5b87
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-16 14:54:22 +0900

    アクティブストレージ導入に不足していた部分

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,11 @@ class ItemsController < ApplicationController
   def  new
     @user = User.new
   end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
