7f5aac9168b919f4655b45bcb157c14c15cb8041
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-17 18:34:28 +0900

    価格情報の入力に関するバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :description, presence: true
-  validates :category, presence: true
-  
+  validates :price, presence: true, format: { with: /\A[3-9]\d{2,6}\z/, message: "は¥300以上¥9,999,999以下で入力してください" }
+
   has_one_attached :image
 end
