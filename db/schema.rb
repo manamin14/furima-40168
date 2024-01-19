279656887ead0be2422dab4f4274ac504d74a12f
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-19 11:20:02 +0900

    不要なマイグレーションの削除を行なった

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_16_063130) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_053125) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,16 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_16_063130) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "articles", charset: "utf8", force: :cascade do |t|
-    t.integer "category_id", null: false
-    t.integer "detail_id", null: false
-    t.integer "shipping_detail_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "shipping_day_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
