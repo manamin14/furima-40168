724eda06264b4528423f3f4e2f5a92c66202d643
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-19 18:01:36 +0900

    エラーハンドリングが表示されるよう実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, format: { with: /\A[3-9]\d{2,6}\z/, message: "は¥300以上¥9,999,999以下で入力してください" }
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :detail_id, numericality: { other_than: 1 }
-  validates :shipping_detail_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :shipping_day_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :detail_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_detail_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_day_id, numericality:{ other_than: 1, message: "can't be blank"}
 
   has_one_attached :image
   belongs_to :category
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+  title         { 'Sample Item' }
+  description   { 'This is a sample item.' }
+  detail_id { 2 }
+  category_id { 2 }
+  shipping_detail_id { 2 }
+  shipping_day_id { 2 }
+  prefecture_id { 2 }
+  price { 500 }
+
+  after(:build) do |item|
+    item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+  end
   end
 end
