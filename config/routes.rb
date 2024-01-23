f99718fbf8d4977a2bbc9364475c3fe29005a8ba
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-23 17:06:08 +0900

    自分が出品していない販売中の商品の購入画面に遷移することができる

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items, only: [:index, :new,:create,:show, :edit, :update, :destroy]
-  
-  resources :orders, only:[:create]
+
+  resources :orders, only:[:index, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
