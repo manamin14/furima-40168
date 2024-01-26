149f30690e8e44497fde998d7babe86977a22d80
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-26 14:28:53 +0900

    ログイン状態でも自分が出品した商品であればトップページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -23,5 +24,11 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city_name, :street_address, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def move_to_index
+    if user_signed_in? && @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
