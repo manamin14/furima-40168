4404cfa7b40044250f970f224dc3fecd408359f6
manamin14 <151704924+manamin14@users.noreply.github.com>
2024-01-26 12:32:17 +0900

    ログアウト状態では商品購入ページに遷移しようとするとログインページに遷移する記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
   before_action :move_to_index, only: [:index]
 
